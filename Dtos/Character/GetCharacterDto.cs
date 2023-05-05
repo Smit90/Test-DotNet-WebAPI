d4fefb40fd1aa80c651ca29b80e42ff2a4e08997
Smit90 <45687440+Smit90@users.noreply.github.com>
2023-05-05 10:43:12 +0530

    Add Skills to Character

--- Dtos/Character/GetCharacterDto.cs
+++ Dtos/Character/GetCharacterDto.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using dotnet_rpg.Dtos.Skill;
 using dotnet_rpg.Dtos.Weapon;
 
 namespace dotnet_rpg.Dtos.Character
@@ -18,5 +19,6 @@ namespace dotnet_rpg.Dtos.Character
         public RpgClass Class { get; set; } = RpgClass.Knight;
 
         public GetWeaponDto? Weapon { get; set; }
+        public List<GetSkillDto>? Skills { get; set; }
     }
 }
